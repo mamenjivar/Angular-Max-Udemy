b31cd2857ba80fcb9488c8b82c07ee610b75da61
Miguel <menjivarmiguel221@gmail.com>
2022-01-22 23:00:56 -0800

    pushed new changes to project folder

--- Course-Project/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ Course-Project/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from "../recipe.model";
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a test', 'https://th-thumbnailer.cdn-si-edu.com/4Nq8HbTKgX6djk07DqHqRsRuFq0=/1000x750/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/d5/24/d5243019-e0fc-4b3c-8cdb-48e22f38bff2/istock-183380744.jpg')
+  ];
 
   constructor() { }
 
